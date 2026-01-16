55b84c6961219ec13df51bed8f2853122176278a
Jeremiah (Ogochukwu) Ngiri <jerrydben24@gmail.com>
2026-01-16 14:41:50 +0100

    Add SQL script for change over time analysis

---
+++ Scripts/exploratory_data_analysis/change_over_time.sql
@@ -0,0 +1,16 @@
+/*
+===============================================================================
+1. Change Over Time Analysis
+===============================================================================
+Objective: Track total sales, unique customers, and quantity sold per year.
+*/
+
+SELECT 
+    YEAR(order_date) AS order_year,
+    SUM(sales_amount) AS total_amount,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date)
+ORDER BY order_year;
