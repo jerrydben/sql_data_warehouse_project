7cfab7ff4d2b440a20795a4c61d000453a0c3e72
Jeremiah (Ogochukwu) Ngiri <jerrydben24@gmail.com>
2026-01-16 14:42:54 +0100

    Create cumulative_analysis.sql

---
+++ Scripts/exploratory_data_analysis/cumulative_analysis.sql
@@ -0,0 +1,27 @@
+/*
+===============================================================================
+2. Cumulative (Running Total) Analysis
+===============================================================================
+Objective: Calculate the running total of sales and the moving average of price.
+Optimized using CTE for better readability compared to subqueries.
+*/
+
+WITH MonthlyAggregates AS (
+    SELECT
+        DATETRUNC(year, order_date) AS order_year,
+        SUM(sales_amount) AS total_sales,
+        AVG(price) AS avg_price
+    FROM gold.fact_sales
+    WHERE order_date IS NOT NULL
+    GROUP BY DATETRUNC(year, order_date)
+)
+SELECT
+    order_year,
+    total_sales,
+    avg_price,
+    -- Cumulative sales calculation
+    SUM(total_sales) OVER (ORDER BY order_year) AS running_total_sales,
+    -- Cumulative average price calculation
+    AVG(avg_price) OVER (ORDER BY order_year) AS running_avg_price
+FROM MonthlyAggregates
+ORDER BY order_year;
